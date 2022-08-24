b72acc355284b97a5a13b78bd1283a6da1e4123b
Emad-Hereba <tuguiaegipcio@gmail.com>
2022-08-24 13:51:27 +0200

    1 file modified

--- src/query.js
+++ src/query.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 const Query = () => {
+  let text ="";
   const names =[
     "Emad Hereba ",
     "Emad Radwan"
-  ]
-  return (<div>
-    {names}
-    </div>
+  ];
+  for (let i = 0; i<names.length; i++){
+    text+= `names[i]+<br>`
+  }
+  text=names
+  return (
+   <div>
+     {names}
+   </div>
   );
 };
 
