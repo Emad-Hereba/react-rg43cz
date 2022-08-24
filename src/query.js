33b29e8a5d4727c7bb1f18b2c21d0d4a2a329f4c
Emad-Hereba <tuguiaegipcio@gmail.com>
2022-08-24 14:07:16 +0200

    1 file modified

--- src/query.js
+++ src/query.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
 const Query = () => {
-  let text='';
-  const names =[
-    "Emad Hereba",
-    "Emad Radwan"
-  ]
-  for (let i=0; i<names.length; i++)
-  text += names[i] +`<br>`
+  let text = '';
+  const names = [
+    'Emad Hereba',
+    'Emad Radwan',
+    'Yahia Emad Hereba',
+    'Ahmed Emad Hereba',
+    'Youssif Emad Radwan',
+  ];
+  for (let i = 0; i < names.length; i++) text += names[i] + '<br/>';
   return (
     <div>
-      {text}
+      {names.map((s) => (
+        <React.Fragment>
+          {s}
+          <br />
+        </React.Fragment>
+      ))}
     </div>
   );
 };
