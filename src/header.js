648457735ee92e30b774c72c18d42688790f88e5
Emad-Hereba <tuguiaegipcio@gmail.com>
2022-08-24 15:02:35 +0200

    1 file modified

--- src/header.js
+++ src/header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div>
-      <h1>Hello Senor Emad!</h1>
+      <h1>Hello Senor Emad !!!!!</h1>
       <p>Start removing from Array :)</p>
     </div>
   );
