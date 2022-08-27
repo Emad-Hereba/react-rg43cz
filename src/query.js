2931a32617cf7995c5559a2b660391958f8e3873
Emad-Hereba <tuguiaegipcio@gmail.com>
2022-08-27 13:00:41 +0200

    1 file modified

--- src/query.js
+++ src/query.js
@@ -13,6 +13,7 @@ const Query = () => {
   // for (let i = 0; i < names.length; i++) text += names[i] + '<br/>';
   const removeit = () => {
     console.log(names);
+    alert(names.lenght-1);
     setNames(names.slice(names.lenght-1,1));
     alert(names);
   };
