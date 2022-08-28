8a24dda2a42b3afebdf58554285d30e4886bed85
Emad-Hereba <tuguiaegipcio@gmail.com>
2022-08-28 16:16:18 +0200

    1 file modified, 1 deleted

--- src/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-componentDidMount() {
-  axios.get(`https://jsonplaceholder.typicode.com/users`)
-    .then(res => {
-      const persons = res.data;
-      this.setState({ persons });
-    })
-}
