08d4cfbcc066b84cadee789bcb00c87082045998
Emad-Hereba <tuguiaegipcio@gmail.com>
2022-08-28 16:17:34 +0200

    1 file modified

--- src/removing.js
+++ src/removing.js
@@ -33,7 +33,7 @@ const Removit = () => {
   //  alert(names);
   };
   const callaxios = ()=>{
-    axios.get(`http://127.0.0.1:8080/db.json`)
+    axios.get(` http://192.168.1.104:8080/db.json`)
   }
   return (
     <div>
